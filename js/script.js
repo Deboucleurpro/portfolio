2e02ce6cd7db5c20ad7f89d40a26750a355ce7f0
Valentinoov <valentin.martine@etu.u-paris.fr>
2025-05-19 19:14:34 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,9 @@
-Script
+// Scroll fluide pour les liens de navigation
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener("click", function (e) {
+    e.preventDefault();
+    document.querySelector(this.getAttribute("href")).scrollIntoView({
+      behavior: "smooth"
+    });
+  });
+});
